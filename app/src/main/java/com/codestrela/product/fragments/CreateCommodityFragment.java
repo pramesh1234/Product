ba219bc7a87e043c8c70bd8bc8750595529d8d58
pramesh1234 <prameshsingh2411@gmail.com>
2020-05-01 03:31:36 +0530

    updated

--- app/src/main/java/com/codestrela/product/fragments/CreateCommodityFragment.java
+++ app/src/main/java/com/codestrela/product/fragments/CreateCommodityFragment.java
@@ -9,6 +9,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import com.codestrela.product.R;
 import com.codestrela.product.base.activity.BaseActivity;
@@ -18,9 +22,10 @@ import com.codestrela.product.viewmodels.CreateCommodityViewModel;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class CreateCommodityFragment extends Fragment {
+public class CreateCommodityFragment extends Fragment{
     CreateCommodityViewModel vm;
     FragmentCreateCommodityBinding binding;
+    RadioButton modeRadioBtn;
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,7 +39,42 @@ public class CreateCommodityFragment extends Fragment {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_create_commodity, container, false);
         binding.setVm(vm);
         ((BaseActivity) getActivity()).setToolbarVisibility(false);
-        return binding.getRoot();
+
+        RadioGroup modeRadioGroup=(RadioGroup) binding.getRoot().findViewById(R.id.rgMode);
+        modeRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId){
+                    case R.id.rentId:
+                              vm.mode.set("rent");
+                        Toast.makeText(getContext(), "rent", Toast.LENGTH_SHORT).show();
+                              break;
+                    case R.id.saleId:
+                                vm.mode.set("sale");
+                        Toast.makeText(getContext(), "sale", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        RadioGroup typeRadioGroup=(RadioGroup) binding.getRoot().findViewById(R.id.rgType);
+        typeRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId){
+                    case R.id.publicRb:
+                        vm.type.set("public");
+                        Toast.makeText(getContext(), "public", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.privateRb:
+                        vm.type.set("private");
+                        Toast.makeText(getContext(), "private", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.groupRb:
+                        vm.type.set("group");
+                        Toast.makeText(getContext(), "group", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+       return binding.getRoot();
     }
     public static void addFragment(BaseActivity activity) {
         activity.replaceFragment(new CreateCommodityFragment(), false);
--- app/src/main/java/com/codestrela/product/viewmodels/CreateCommodityViewModel.java
+++ app/src/main/java/com/codestrela/product/viewmodels/CreateCommodityViewModel.java
@@ -1,10 +1,49 @@
 package com.codestrela.product.viewmodels;
 
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
 import com.codestrela.product.fragments.CreateCommodityFragment;
+import com.codestrela.product.util.BindableBoolean;
+import com.codestrela.product.util.BindableString;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class CreateCommodityViewModel {
+    public BindableString name=new BindableString();
+    public BindableString unit=new BindableString();
+    public BindableString price=new BindableString();
+    public BindableString spection=new BindableString();
+    public BindableString mode=new BindableString();
+    public BindableString type=new BindableString();
+    public FirebaseFirestore db;
+    public FirebaseAuth mAuth;
+    RadioGroup modeRadioGroup;
     CreateCommodityFragment createCommodityFragment;
     public CreateCommodityViewModel(CreateCommodityFragment createCommodityFragment) {
         this.createCommodityFragment=createCommodityFragment;
+        db=FirebaseFirestore.getInstance();
+        mAuth=FirebaseAuth.getInstance();
+    }
+    public CreateCommodityViewModel(RadioGroup modeRadioGroup) {
+       this.modeRadioGroup=modeRadioGroup;
+    }
+    public void onSubmit(View view){
+        String userId=mAuth.getUid();
+        Map<String, Object> commodity = new HashMap<>();
+        commodity.put("name", name.get());
+        commodity.put("unit", unit.get());
+        commodity.put("price", price.get());
+        commodity.put("spection",spection.get());
+        commodity.put("mode",mode.get());
+        commodity.put("type",type.get());
+
+        db.collection("users").document(userId).collection("commodity_list").document().set(commodity);
+
     }
 }
